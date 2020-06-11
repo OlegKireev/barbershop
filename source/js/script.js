40407dc2ce2f7486159c4646c3414ed6c12821a8
Flucky <flucky@mail.ru>
2020-06-11 19:56:27 +0300

    Первичная стилизация планшетной версии всех блоков index.html. Переработка скрипта слайдера коментариев, под планшетную версию

--- source/js/script.js
+++ source/js/script.js
@@ -1,130 +1,150 @@
 // Меню
-
-// const tabletWidth = 768;
-// const menu = document.querySelector('.menu');
-// const menuItems = menu.querySelectorAll('li');
-// const menuButton = menu.querySelector('.menu__button');
-// const menuIcon = menu.querySelector('.menu__icon');
-
-// // ПРИ ИЗМЕНЕНИИ РАЗМЕРОВ ОКНА ТАКАЯ РЕАЛИЗАЦИЯ СКРЫТИЯ/ПОКАЗА МЕНЮ НЕ РАБОТАЕТ
-// menuButton.addEventListener('click', () => {
-//     menuItems.forEach((item,i)=> {
-//         menu.classList.toggle('menu--closed');
-//         menuButton.classList.toggle('menu__button--opened');
-//         menuIcon.classList.toggle('menu__icon--opened');
-
-//             if (i > 0) {
-//                 item.classList.toggle('display-none');
-//             } else {
-//                 item.querySelector('a').classList.toggle('menu__link--active');
-//             }
-
-//     })
-// });
+(() => {
     
+    const menuButton = document.querySelector('.menu__button');
+    const menuButtonIcon = menuButton.querySelector('.menu__icon');
+    const menuList = document.querySelector('.menu__list');
+    const menuCurrent = document.querySelector('.menu__current');
+    let currentLinkTextBack = menuCurrent.textContent;
+    
+    menuButton.addEventListener('click', () => {
+        menuButton.classList.toggle('menu__button--opened');
+        menuList.classList.toggle('menu__list--opened');
+        menuButtonIcon.classList.toggle('menu__icon--opened');
+    
+        if (menuCurrent.textContent === ``) {
+            menuCurrent.textContent = currentLinkTextBack;
+        } else {
+            menuCurrent.textContent = ``;
+        }
+    })
 
-// // Закрываем меню при загрузке
-// document.addEventListener('DOMContentLoaded', () => {
-//     setTimeout(() => {
-//         menuButton.click();
-//     }, 0);
-// });
+})();
 
-const menuButton = document.querySelector('.menu__button');
-const menuButtonIcon = menuButton.querySelector('.menu__icon');
-const menuList = document.querySelector('.menu__list');
-const menuCurrent = document.querySelector('.menu__current');
-let currentLinkTextBack = menuCurrent.textContent;
-
-menuButton.addEventListener('click', () => {
-    menuButton.classList.toggle('menu__button--opened');
-    menuList.classList.toggle('menu__list--opened');
-    menuButtonIcon.classList.toggle('menu__icon--opened');
-
-    if (menuCurrent.textContent === ``) {
-        menuCurrent.textContent = currentLinkTextBack;
-    } else {
-        menuCurrent.textContent = ``;
-    }
-})
 
 // Слайдер с преимуществами
 // СДЕЛАТЬ ПЛАВНУЮ АНИМАЦИЮ СМЕНЫ СЛАЙДОВ
-const advantages = document.querySelector('.advantages');
-const slides = advantages.querySelectorAll('.advantage');
-const sliderButtons = advantages.querySelectorAll('.advantages__dot');
+(()=>{
 
-sliderButtons.forEach((button, i)=> {
-    button.addEventListener('click', (evt) => {
-
-        // Скрываем все карточки
-        slides.forEach((slide, i) => {
-            slide.classList.remove('advantage--active');
-            // slide.classList.add('display-none');
-        })
-
-        // Показываем нужную карточку
-        // slides[i].classList.remove('display-none');
-        slides[i].classList.add('advantage--active');
-        
-        sliderButtons.forEach((btn, i) => {
-            // Удаляем активный класс всех кнопок
-            btn.classList.remove('advantages__dot--active');
-        });
-
-        // Добавляем активный класс нажатой кнопке
-        evt.target.classList.add('advantages__dot--active');
-
-    });
-});
-
-// Автолистание слайдера
-// СДЕЛАТЬ СБРОС СЧЕТЧИКА ПРИ КЛИКЕ И ПОЛОСУ ПРОГРЕССА
-document.addEventListener('DOMContentLoaded', () => {
-    let i = 0;
-
-    setInterval(() => {
-        if (i >= slides.length - 1) {
-            i = 0;
-        } else {
-            i++;
-        }
-        sliderButtons[i].click();
-    }, 10000);
+    const advantages = document.querySelector('.advantages');
+    const slides = advantages.querySelectorAll('.advantage');
+    const sliderButtons = advantages.querySelectorAll('.advantages__dot');
     
-});
-
-
-// Слайдер с коментариями
-const container = document.querySelector('.reviews');
-const reviews = container.querySelectorAll('.review');
-const reviewsButtons = container.querySelectorAll('.reviews__dot');
-
-reviewsButtons.forEach((button, i)=> {
-    button.addEventListener('click', (evt) => {
-
-        reviews.forEach((slide, i) => {
-            slide.classList.remove('review--active');
-            slide.classList.add('display-none');
-        })
-
-        reviews[i].classList.remove('display-none');
-        reviews[i].classList.add('review--active');
-
-        reviewsButtons.forEach((btn, i) => {
-            btn.classList.remove('reviews__dot--active');
+    sliderButtons.forEach((button, i)=> {
+        button.addEventListener('click', (evt) => {
+    
+            // Скрываем все карточки
+            slides.forEach((slide, i) => {
+                slide.classList.remove('advantage--active');
+            })
+    
+            // Показываем нужную карточку
+            slides[i].classList.add('advantage--active');
+            
+            sliderButtons.forEach((btn, i) => {
+                // Удаляем активный класс всех кнопок
+                btn.classList.remove('advantages__dot--active');
+            });
+    
+            // Добавляем активный класс нажатой кнопке
+            evt.target.classList.add('advantages__dot--active');
+    
         });
-
-        evt.target.classList.add('reviews__dot--active');
-
     });
-});
+    
+    // Автолистание слайдера
+    // СДЕЛАТЬ СБРОС СЧЕТЧИКА ПРИ КЛИКЕ И ПОЛОСУ ПРОГРЕССА
+    document.addEventListener('DOMContentLoaded', () => {
+        let i = 0;
+    
+        setInterval(() => {
+            if (i >= slides.length - 1) {
+                i = 0;
+            } else {
+                i++;
+            }
+            sliderButtons[i].click();
+        }, 10000);
+        
+    });
+
+})();
+
+(()=>{
+
+    // Слайдер с коментариями
+    const container = document.querySelector('.reviews');
+    const reviews = container.querySelectorAll('.review');
+    const reviewsButtons = container.querySelectorAll('.reviews__dot');
+    const previousButton = container.querySelector('.reviews__button--previous');
+    const nextButton = container.querySelector('.reviews__button--next');
+    // Переменная активной карточки коментария
+    let activeTabIndex = 0;
+    
+    // Вешаем обрабочики на все дот-кнопки
+    reviewsButtons.forEach((button, i)=> {
+        button.addEventListener('click', (evt) => {
+    
+            // Прячем все карточки коментариев
+            reviews.forEach((slide) => {
+                slide.classList.remove('review--active');
+            })
+    
+            // Показываем нужную
+            reviews[i].classList.add('review--active');
+            // Устанавливаем переменную равную той дот-кнопке
+            // для синхронизации с кнопками "влево", "вправо"
+            activeTabIndex = i;
+    
+            // Удаляем активный класс у всех дот-кнопок
+            reviewsButtons.forEach((btn) => {
+                btn.classList.remove('reviews__dot--active');
+            });
+    
+            // Добавляем активный класс у кликнутой кнопки
+            evt.target.classList.add('reviews__dot--active');
+    
+        });
+    });
+    
+    // Добавляем обработчик на кнопку "влево"
+    nextButton.addEventListener('click', () => {
+        
+        // Если кнопка не последняя
+        if (activeTabIndex < reviewsButtons.length - 1) {
+            activeTabIndex++;
+        // Если кнопка последняя
+        } else {
+            activeTabIndex = 0;
+        }
+        // Кликаем по нужной кнопке
+        reviewsButtons[activeTabIndex].click();
+    
+    });
+    
+    previousButton.addEventListener('click', () => {
+    
+        // Если кнопка первая
+        if (activeTabIndex === 0) {
+            activeTabIndex = reviewsButtons.length - 1;
+        // Если кнопка не первая 
+        } else {
+            activeTabIndex--;
+        }
+        // Кликаем по нужной кнопке
+        reviewsButtons[activeTabIndex].click();
+    
+    });
+
+})();
+
+
 
 // Прячем все отзывы кроме первого
-document.addEventListener('DOMContentLoaded', () => {
-    reviews.forEach((item, i) => {
-        if(i > 0) {
-            item.classList.add('display-none');
-        }
-    })
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     reviews.forEach((item, i) => {
+//         if(i > 0) {
+//             item.classList.add('display-none');
+//         }
+//     })
+// });
