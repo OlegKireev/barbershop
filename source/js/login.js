c43d4d6b65800f470ca74047f13dae5d3533d379
Flucky <flucky@mail.ru>
2020-06-28 17:09:10 +0300

    Переработана верстка модальных окон и скрипт их открытия.

--- source/js/login.js
+++ source/js/login.js
@@ -1,34 +1,34 @@
-// Логин
-(() => {
-    const loginButton = document.querySelector(`.menu__link--login`);
-    const loginModal = document.querySelector(`.login`);
-    const modalCloseButton = loginModal.querySelector(`.popup__button--close`);
-    const pageBody = document.querySelector(`body`);
-    const ESC__BUTTON = 27;
+// // Логин
+// (() => {
+//     const loginButton = document.querySelector(`.menu__link--login`);
+//     const loginModal = document.querySelector(`.login`);
+//     const modalCloseButton = loginModal.querySelector(`.popup__button--close`);
+//     const pageBody = document.querySelector(`body`);
+//     const ESC__BUTTON = 27;
     
-    const showLoginModal = function () {
-        loginModal.classList.remove(`display-none`);
-        // pageBody.classList.add(`overflow-hidden`);
-    };
+//     const showLoginModal = function () {
+//         loginModal.classList.remove(`visually-hidden`);
+//         pageBody.classList.add(`overflow-hidden`);
+//     };
 
-    const hideLoginModal = function () {
-        loginModal.classList.add(`display-none`);
-        // pageBody.classList.remove(`overflow-hidden`);
-        document.removeEventListener(`keydown`, onEscCloseModal);
-    };
+//     const hideLoginModal = function () {
+//         loginModal.classList.add(`visually-hidden`);
+//         pageBody.classList.remove(`overflow-hidden`);
+//         document.removeEventListener(`keydown`, onEscCloseModal);
+//     };
 
-    const onEscCloseModal = function (evt) {
-        if (evt.keyCode === ESC__BUTTON) {
-            hideLoginModal();
-        }
-    }
+//     const onEscCloseModal = function (evt) {
+//         if (evt.keyCode === ESC__BUTTON) {
+//             hideLoginModal();
+//         }
+//     }
 
-    loginButton.addEventListener(`click`, () => {
-        showLoginModal();
-        document.addEventListener(`keydown`, onEscCloseModal);
-    })
+//     loginButton.addEventListener(`click`, () => {
+//         showLoginModal();
+//         document.addEventListener(`keydown`, onEscCloseModal);
+//     })
 
-    modalCloseButton.addEventListener(`click`, () => {
-        hideLoginModal();
-    })
-})();
+//     modalCloseButton.addEventListener(`click`, () => {
+//         hideLoginModal();
+//     })
+// })();
